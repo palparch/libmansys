237ddd2ba68ff2cbe6860a91f2aa5c65addb8b72
palparch <tuilipshrm2008@gmail.com>
2026-08-01 01:03:07 +0530

    added an outline

---
+++ main.py
@@ -0,0 +1,47 @@
+# Main menu
+
+
+
+
+# Add books
+
+
+# Display books
+
+
+# Search a book
+
+
+# Update a book
+
+
+# Delete
+
+
+# Issue
+
+
+# Return
+
+
+# Show avaiable
+
+
+# Show issued
+
+
+# Count books
+
+
+# Books by genre
+
+
+# Most borrowed books
+
+
+# Top authors
+
+
+# Exit program
+
+
